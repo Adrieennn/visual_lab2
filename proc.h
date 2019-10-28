d35fb9a66304494e17987068025c73e359336a94
kessibi <guillaume@kessibi.fr>
2019-10-28 05:53:12 +0100

    proc: added lab3 functions

--- proc.h
+++ proc.h
@@ -6,5 +6,9 @@ void medianFilter(gray* graymap, int rows, int cols);
 void histogram_building(gray* graymap, int length, int* hist);
 void stretch_histogram(gray* graymap, int min, int max, int maxval, int len);
 void equalize_histogram(gray* graymap, int* histogram, int maxval, int len);
+int* magnitude(float** grads, int length);
+int* nms(float** grads, int* mag, int rows, int cols);
+float** scharr(gray* graymap, int rows, int cols);
+int* hyst(int* mag, int rows, int cols, int max);
 
 #endif
